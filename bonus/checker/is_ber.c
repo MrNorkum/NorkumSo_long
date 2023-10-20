13e84778f7f25239fe69bdb829c73b9cf89693e3
Mr. Norkum <kelestayyib@gmail.com>
2023-10-20 07:57:23 +0300

    Successful

--- bonus/checker/is_ber.c
+++ bonus/checker/is_ber.c
@@ -6,7 +6,6 @@
 void	is_ber(char **av, t_map *map)
 {
 	map->ber = av[1];
-	if (!ft_strrchr(av[1], '.')
-		|| ft_strncmp(ft_strrchr(av[1], '.'), ".ber", 5))
+	if (ft_strncmp(av[1] + ft_strlen(av[1) - 4, ".ber", 5))
 		return (write(2, "Not ber file!!\n", 15), exit(1));
 }
